b6c87d8254615508f7b055eb72d8219f287f5963
Hugh <hugh_wang09@163.com>
2016-04-08 21:50:40 +0800

    enum & dictwrapper

---
+++ dictwrapper/sample.py
@@ -0,0 +1,21 @@
+# coding=utf-8
+
+from dictwrapper import DictWrapper
+
+if __name__ == "__main__":
+
+    book_list=[
+        {'name': 'Guide to python',
+         'author': {'name': 'Hugh', 'gender': 'male'},
+         'pages': 219 },
+
+        {'name': 'The Three Kindoms',
+         'author': {'name': 'Luo GuanZhong', 'gender': 'male'},
+         'pages': 133}
+    ]
+
+    books=DictWrapper.load_dict(book_list)
+
+    for book in books:
+        print book.name, book.author.name
+
---
+++ enum/sample.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+
+from enum import EnumType, BaseEnum
+
+class GenderEnum(BaseEnum):
+
+    male=EnumType(1, u'男')
+    female=EnumType(0, u'女')
+    unknown=EnumType(-1, u'未知')
+
+
+if __name__ == '__main__':
+
+    print GenderEnum.male
+    print GenderEnum.choices
+    print GenderEnum.display(0)
